275a091bdab6c024c7196d7d778a11360ec07f3e
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2023-06-04 16:30:59 +0300

    DB redesign

--- backend/Products/models.py
+++ backend/Products/models.py
@@ -11,15 +11,15 @@ from django.db import models
 
 
 # for storing product details
-class ProductDetail:
+class ProductDetail(models.Model):
     product_id = models.AutoField(primary_key=True, unique=True)
-    product_name = models.TextField(unique=True, max_length=255)
+    product_name = models.CharField(unique=True, max_length=255)
     min_selling_price = models.DecimalField(max_digits=7, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 # maintaining the stock level
-class StockLevel:
+class StockLevel(models.Model):
     product_id = models.ForeignKey(ProductDetail, on_delete=models.CASCADE)
     available_units = models.IntegerField()
     min_units_alert = models.IntegerField()
@@ -27,7 +27,7 @@ class StockLevel:
 
 
 # for storing purchase history
-class PurchaseHistory:
+class PurchaseHistory(models.Model):
     product_id = models.ForeignKey(ProductDetail, on_delete=models.CASCADE)
     units = models.IntegerField()
     buying_price = models.DecimalField(max_digits=7, decimal_places=2)
@@ -35,7 +35,7 @@ class PurchaseHistory:
 
 
 # for storing sales history
-class Sales:
+class Sales(models.Model):
     product_id = models.ForeignKey(ProductDetail, on_delete=models.CASCADE)
     units = models.IntegerField()
     amount = models.DecimalField(max_digits=7, decimal_places=2)
