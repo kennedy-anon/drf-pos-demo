78da7cdce11d782df341028972c1babf5b028491
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2023-06-04 18:24:41 +0300

    updating stock level after a purchase

--- backend/Products/models.py
+++ backend/Products/models.py
@@ -1,15 +1,5 @@
 from django.db import models
 
-# # to be cleared
-# class Product(models.Model):
-#     product_id = models.AutoField(primary_key=True, unique=True)
-#     product_name = models.TextField(unique=True, max_length=255)
-#     quantity = models.IntegerField()
-#     buying_price = models.DecimalField(max_digits=7, decimal_places=2)
-#     min_selling_price = models.DecimalField(max_digits=7, decimal_places=2)
-#     created_at = models.DateTimeField(auto_now_add=True)
-
-
 # for storing product details
 class ProductDetail(models.Model):
     product_id = models.AutoField(primary_key=True, unique=True)
--- backend/Products/serializers.py
+++ backend/Products/serializers.py
@@ -43,28 +43,11 @@ class PurchaseHistorySerializer(serializers.Serializer):
     def create(self, validated_data):
         product_id = validated_data.pop('product_id')
 
-        return PurchaseHistory.objects.create(product_id=product_id, **validated_data)
+        purchase_history =  PurchaseHistory.objects.create(product_id=product_id, **validated_data)
 
-# # serializes the products
-# class ProductSerializer(serializers.Serializer):
-#     product_name = serializers.CharField(max_length=255, min_length=1, allow_blank=False, trim_whitespace=True)
-#     quantity = serializers.IntegerField()
-#     buying_price = serializers.DecimalField(max_digits=7, decimal_places=2)
-#     min_selling_price = serializers.DecimalField(max_digits=7, decimal_places=2)
+        # update the stock level
+        stock_level = StockLevel.objects.get(product_id=product_id)
+        stock_level.available_units += purchase_history.units
+        stock_level.save()
 
-#     class Meta:
-#         model = Product
-#         fields = [
-#             'product_id',
-#             'product_name',
-#             'quantity',
-#             'buying_price',
-#             'min_selling_price',
-#             'created_at'
-#         ]
-
-#     def create(self, validated_data):
-#         try:
-#             return Product.objects.create(**validated_data)
-#         except IntegrityError:
-#             raise serializers.ValidationError("Duplicate entry. This item already exists.")
+        return purchase_history
